Fixed utilization calculation, improved take-overs

manager.py
@@ -7,6 +7,9 @@ import math
 import shapely
 import numpy as np
 
+import logging
+logger = logging.getLogger(__name__)
+
 
 class AgentType:
     def __init__(self, model: str, values: dict):
@@ -23,10 +26,8 @@ class AgentType:
         self.color = values["color"]
         self.team = values["team"]
 
-        self.ingress_distance = 2000
-        self.ideal_utilisation = (self.endurance / self.speed) / ((self.endurance / self.speed) + self.maintenance)
-        self.trip_time = (self.endurance / self.speed) - self.ingress_distance
-        self.concurrent_agents = int(np.floor(self.quantity * self.ideal_utilisation))
+        self.ingress_distance = settings.AREA_WIDTH + abs(settings.BASE_X)
+        self.concurrent_locations = self.calculate_concurrent_locations()
 
         self.patrol_locations = []
         self.activation_queue = []
@@ -36,6 +37,14 @@ class AgentType:
                 f"Active: {len(self.active_agents)}, "
                 f"Inactive: {len(self.inactive_agents)}, "
                 f"Maint: {len(self.maintenance_agents)}")
+
+    def calculate_concurrent_locations(self) -> int:
+        ingress_time = self.ingress_distance / self.speed
+        downtime = self.maintenance + 2 * ingress_time
+        uptime = (self.endurance - 2*self.ingress_distance) / self.speed
+        required = np.ceil(downtime / uptime) + 1
+        print(f"{self.model} has {int(np.floor(self.quantity / required))} concurrent locations")
+        return int(np.floor(self.quantity / required))
 
     def create_patrol_location(self) -> points.PatrolLocation:
         x_coord = np.random.uniform(0, settings.AREA_WIDTH)
@@ -49,19 +58,29 @@ class AgentType:
 
         self.ingress_distance = max(location.distance_to(points.Point(settings.BASE_X, settings.BASE_Y)),
                                     self.ingress_distance)
-        print(f"Model {self.model} has {len(self.patrol_locations)} patrol locations for {self.concurrent_agents} agents.")
+        print(f"Model {self.model} has {len(self.patrol_locations)} patrol locations "
+              f"for {self.concurrent_locations} agents.")
         return location
 
     def update_agents(self) -> None:
+
+        for agent in copy.copy(self.maintenance_agents):
+            completed = agent.update_maintenance()
+            if completed:
+                self.maintenance_agents.remove(agent)
+                self.inactive_agents.append(agent)
+
         active_agents = copy.copy(self.active_agents)
         for agent in active_agents:
+            current_ingress_distance = agent.location.distance_to(agent.base)
+
             if not agent.called_replacement:
-                if agent.remaining_endurance < 2 * self.ingress_distance:
+                if agent.remaining_endurance < 2 * current_ingress_distance:
                     self.call_next_agent(patrol_location=agent.patrol_location)
                     agent.called_replacement = True
 
             if not agent.returning:
-                if agent.remaining_endurance < self.ingress_distance:
+                if agent.remaining_endurance < current_ingress_distance * 1.1:
                     agent.return_to_base()
 
             agent.move_through_route()
@@ -70,10 +89,10 @@ class AgentType:
                 if agent.location != agent.base:
                     raise ValueError("Agent out of base with remaining maintenance")
                 self.active_agents.remove(agent)
+                self.maintenance_agents.append(agent)
 
     def plot_agents(self, ax):
         for agent in self.active_agents:
-            print(f"Plotting {agent} at {agent.location.x, agent.location.y}")
             if agent.plot_object is None:
                 agent.plot_object = ax.scatter(agent.location.x, agent.location.y,
                                                color=agent.patrol_location.color, marker="X", zorder=2,
@@ -83,7 +102,10 @@ class AgentType:
 
     def call_next_agent(self, patrol_location: points.PatrolLocation) -> None:
         if len(self.inactive_agents) == 0:
-            raise ValueError(f"No inactive agents available - agents in maint: {self.maintenance_agents}")
+            for pl in self.patrol_locations:
+                logger.error(pl)
+            raise ValueError(f"No inactive agents available for {self.model} "
+                             f"- agents in maint: {self.maintenance_agents}")
         next_agent = self.inactive_agents.pop()
         self.active_agents.append(next_agent)
         next_agent.activate(patrol_location)
@@ -122,7 +144,7 @@ class SearchManager(Manager):
 
     def create_patrol_tessellation(self) -> None:
         for at in self.agent_types:
-            for _ in range(at.concurrent_agents):
+            for _ in range(at.concurrent_locations):
                 self.patrol_locations.append(at.create_patrol_location())
         self.normalize_strength()
         self.distribute_patrol_locations()
@@ -149,7 +171,7 @@ class SearchManager(Manager):
         for at in self.agent_types:
             for pl in at.patrol_locations:
                 at.call_next_agent(pl)
-        print(f"Created {len(self.patrol_locations)} patrol locations")
+        logger.info(f"Created {len(self.patrol_locations)} patrol locations")
 
     def update_patrol_assignments(self) -> None:
         # TODO: Think about whether we should assign points outside the area of interest